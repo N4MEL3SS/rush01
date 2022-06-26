e17c9dac4759d62bae4c4a5224c2e8a5e1a2ac34
celadia <celadia@student.21-school.ru>
2022-06-26 06:35:55 +0300

    Initial commit

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+	public Transform Target;
+	
+	public float DistanceMin = 10;
+	public float DistanceMax = 50;
+	public float SensitivityX = 40;
+	public float SensitivityY = 120;
+	
+	public float yMinLimit = -20f;
+	public float yMaxLimit = 80f;
+	
+	private float _currentDistance;
+	private float _xCamera;
+	private float _yCamera;
+	
+	private RaycastHit _hit;
+	
+	private void Start ()
+	{
+		var angles = transform.eulerAngles;
+		_xCamera = angles.y;
+		_yCamera = angles.x;
+		_currentDistance = 10f;
+	}
+
+	private void LateUpdate()
+	{
+		if (!Target) return;
+		
+		if(Input.GetKey("mouse 1"))
+		{
+			Cursor.visible = false;
+			_xCamera += Input.GetAxis("Mouse X") * SensitivityX * _currentDistance * 0.02f;
+			_yCamera -= Input.GetAxis("Mouse Y") * SensitivityY * 0.02f;
+		}
+		else
+			Cursor.visible = true;
+		
+		_currentDistance = Mathf.Clamp(_currentDistance - Input.GetAxis("Mouse ScrollWheel") * 5, DistanceMin, DistanceMax);
+		
+		if (Physics.Linecast(Target.position, transform.position, out _hit))
+		{
+			if (!_hit.transform.CompareTag("Enemy"))
+				_currentDistance -= _hit.distance;
+		}
+
+		var rotation = Quaternion.Euler(ClampAngle(_yCamera, yMinLimit, yMaxLimit), _xCamera, 0);
+		var position = rotation * new Vector3(0.0f, 0.0f, - _currentDistance) + Target.position;
+		transform.rotation = rotation;
+		transform.position = position;
+	}
+	
+	private static float ClampAngle(float angle, float min,float max) 
+	{
+		if (angle < -360)
+			angle += 360;
+		if (angle > 360)
+			angle -= 360;
+		return Mathf.Clamp(angle, min, max);
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class PlayerMovement : MonoBehaviour
+{
+	public GameObject destPoint;
+	
+	private NavMeshAgent _agent;
+	private RaycastHit _hit;
+	private Camera _mainCamera;
+	private Animator _animator;
+	private GameObject _destPoint;
+
+	public bool isAlive = true;
+	
+	private static readonly int IsWalk = Animator.StringToHash("isWalk");
+
+	private void Awake()
+	{
+		_agent = GetComponent<NavMeshAgent>();
+		_agent.updateRotation = false;
+		_animator = GetComponent<Animator>();
+		_destPoint = Instantiate(destPoint);
+		_mainCamera = Camera.main;
+	}
+
+	private void Update()
+	{
+		if (!isAlive) return;
+		
+		if (Input.GetKey("mouse 0"))
+			PlayerGetTarget();
+		PlayerMovementAnimation();
+	}
+	
+	private void PlayerGetTarget()
+	{
+		if (Physics.Raycast(_mainCamera.ScreenPointToRay(Input.mousePosition), out _hit))
+		{
+			if (_hit.transform.CompareTag("Enemy"))
+			{
+				return;
+			}
+
+			if (_hit.transform.CompareTag("Ground"))
+			{
+				_destPoint.transform.position = new Vector3(_hit.point.x, _hit.point.y, _hit.point.z);
+				_agent.destination = _hit.point;
+			}
+		}
+	}
+	
+	private void PlayerMovementAnimation()
+	{
+		if (_agent.velocity.magnitude <= 0.1f)
+		{
+			_animator.SetBool(IsWalk, false);
+			_agent.updateRotation = false;
+			destPoint.SetActive(false);
+		}
+		else
+		{
+			_animator.SetBool(IsWalk, true);
+			// _agent.updateRotation = true;
+			destPoint.SetActive(true);
+		} 
+	}
+	
+}
